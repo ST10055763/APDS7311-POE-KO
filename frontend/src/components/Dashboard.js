692350608e8a5eee3eb0ea00630521053e868aa6
ST10095103 <ST10095103@vcconnect.edu.za>
2024-10-07 21:24:42 +0200

    logout implemented

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -10,6 +10,13 @@ function Dashboard() {
         firstName: localStorage.getItem("name"), // Retrieve first name from local storage
     };
 
+    const handleLogout =() =>{
+        localStorage.removeItem("name");
+        localStorage.removeItem("jwt");
+
+        navigate("/login");
+    }
+
     return (
         <div className="container mt-5">
             <div className="text-center">
@@ -19,6 +26,13 @@ function Dashboard() {
                     className="btn btn-primary mt-3"
                 >
                     Go to Transaction Page
+                </button>
+
+                <button
+                    onClick={handleLogout}
+                    className="btn btn-danger mt-3 ml-3"
+                >
+                    Logout
                 </button>
             </div>
         </div>
