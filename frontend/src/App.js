dede5a6823f848a824d81364f3b52910285d13fa
Kiashen Maharajh <ST10055763@vcconnect.edu.za>
2024-11-07 20:10:18 +0200

    added submitoswift page for employees

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,6 +8,7 @@ import Dashboard from './components/Dashboard';
 import TransactionPage from './components/TransactionPage.js';
 import ThankYou from './components/ThankYou';
 import EmployeeDash from './components/EmployeeDash.js'
+import SubmitToSwift from './components/SubmitToSwift.js';
 
 
 function App() {
@@ -21,6 +22,7 @@ function App() {
         <Route path="/thank-you" element={<ThankYou/>}/>
         <Route path="/dashboard" element={<Dashboard/>} />
         <Route path="/employee-dash" element={<EmployeeDash/>}/>
+        <Route path="submit-to-swift" element={<SubmitToSwift/>}/>
       </Routes>
     </Router>
   );
--- frontend/src/components/SubmitToSwift.js
+++ frontend/src/components/SubmitToSwift.js
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import './EmployeeDash.css'; // Import the CSS file
+import './SubmitToSwift.css'; // Import the CSS file
 
-function EmployeeDash() {
+function SubmitToSwift() {
     const navigate = useNavigate();
     const [transactions, setTransactions] = useState([]);
     const [loading, setLoading] = useState(true);
@@ -26,7 +26,7 @@ function EmployeeDash() {
                     "Content-Type": "application/json"
                 };
     
-                const response = await fetch('https://localhost:3001/transaction/getpendingtransactions', {
+                const response = await fetch('https://localhost:3001/transaction/getapprovedtransactions', {
                     method: "GET", // Assuming this is a GET request
                     headers: headers, // Add authorization headers
                 });
@@ -59,19 +59,17 @@ function EmployeeDash() {
         <div className="dashboard-container">
             <div className="text-center">
                 <h2>Welcome back Employee, {user.firstName} of KO International Banking!</h2>
-                <h3>Let's Work on Approving or Denying Payment Requests. And Don't Forget to Submit All Approvals to SWIFT!</h3>
+                <h3>Let's Submit All Approvals to SWIFT for Processing!</h3>
                 <div className="button-group">
                 <button 
-    onClick={() => navigate("/submit-to-swift")} 
+    onClick={() => navigate("/employee-dash")} 
     className="btn-orange"
 >
-    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-cash-coin" viewBox="0 0 16 16">
-        <path fillRule="evenodd" d="M11 15a4 4 0 1 0 0-8 4 4 0 0 0 0 8m5-4a5 5 0 1 1-10 0 5 5 0 0 1 10 0"/>
-        <path d="M9.438 11.944c.047.596.518 1.06 1.363 1.116v.44h.375v-.443c.875-.061 1.386-.529 1.386-1.207 0-.618-.39-.936-1.09-1.1l-.296-.07v-1.2c.376.043.614.248.671.532h.658c-.047-.575-.54-1.024-1.329-1.073V8.5h-.375v.45c-.747.073-1.255.522-1.255 1.158 0 .562.378.92 1.007 1.066l.248.061v1.272c-.384-.058-.639-.27-.696-.563h-.668zm1.36-1.354c-.369-.085-.569-.26-.569-.522 0-.294.216-.514.572-.578v1.1zm.432.746c.449.104.655.272.655.569 0 .339-.257.571-.709.614v-1.195z"/>
-        <path d="M1 0a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h4.083q.088-.517.258-1H3a2 2 0 0 0-2-2V3a2 2 0 0 0 2-2h10a2 2 0 0 0 2 2v3.528c.38.34.717.728 1 1.154V1a1 1 0 0 0-1-1z"/>
-        <path d="M9.998 5.083 10 5a2 2 0 1 0-3.132 1.65 6 6 0 0 1 3.13-1.567"/>
-    </svg>
-    Submit to Swift
+<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-house-lock" viewBox="0 0 16 16">
+  <path d="M7.293 1.5a1 1 0 0 1 1.414 0L11 3.793V2.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v3.293l2.354 2.353a.5.5 0 0 1-.708.708L8 2.207l-5 5V13.5a.5.5 0 0 0 .5.5h4a.5.5 0 0 1 0 1h-4A1.5 1.5 0 0 1 2 13.5V8.207l-.646.647a.5.5 0 1 1-.708-.708z"/>
+  <path d="M10 13a1 1 0 0 1 1-1v-1a2 2 0 0 1 4 0v1a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1h-4a1 1 0 0 1-1-1zm3-3a1 1 0 0 0-1 1v1h2v-1a1 1 0 0 0-1-1"/>
+</svg>
+    Return to Dashboard
 </button>
 
 
@@ -94,10 +92,10 @@ function EmployeeDash() {
                 {loading ? (
                     <p>Loading transactions...</p>
                 ) : transactions.length === 0 ? (
-                    <p>No pending transactions found.</p>
+                    <p>No approved transactions waiting to send to SWIFT found</p>
                 ) : (
                     <>
-                        <h3 className="transaction-history-header">Pending Transactions</h3>
+                        <h3 className="transaction-history-header">Approved Transactions</h3>
                         <table className="table">
                             <thead>
                                 <tr>
@@ -134,4 +132,4 @@ function EmployeeDash() {
         </div>
     );
 }
-export default EmployeeDash;
+export default SubmitToSwift;
